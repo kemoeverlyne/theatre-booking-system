2f3434c5e399ee3eb950f0142f86a3050fd7ae48
Everlyne Kemo <KEMOEVERLYNE@GMAIL.COM>
2024-07-16 06:52:15 +0300

    Update settings.py

--- theatre_booking_system/settings.py
+++ theatre_booking_system/settings.py
@@ -132,7 +132,7 @@ SIMPLE_JWT = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -145,3 +145,31 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Redis URL
+REDIS_DB_URL = env.str("REDIS_DB_URL", "redis://localhost:6379")
+
+#CELERY
+CELERY_BROKER_URL = env.str("CELERY_BROKER_URL", REDIS_DB_URL)
+CELERY_RESULT_BACKEND = env.str("CELERY_RESULT_BACKEND", REDIS_DB_URL)
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_ALWAYS_EAGER = env.bool("CELERY_ALWAYS_EAGER", False)
+
+# Celery Beat
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+CELERY_BEAT_SCHEDULE = {
+    "add-every-30-seconds": {
+        "task": "apps.accounts.tasks.add",
+        "schedule": 30.0,
+        "args": (16, 16),
+        "options": {
+            "expires": 15.0,
+        },
+    },
+}
